3df2a67c6d7f9e8db204d7b320a2ba402d85d2b6
Ly Tran Vinh <88392742+Youknow2509@users.noreply.github.com>
2024-09-21 09:08:21 +0000

    [Upgrade]: add route area and rediet route

--- Router.cs
+++ Router.cs
@@ -10,9 +10,23 @@ public class Router {
         app.UseRouting();
 
         app.UseAuthorization();
+        // Route for area
+        app.MapAreaControllerRoute(
+            name: "Identity",
+            areaName: "Identity",
+            pattern: "Identity/{controller=Login}/{action=Index}/{id?}"
+        );
 
+        // Route for basic
         app.MapControllerRoute(
             name: "default",
             pattern: "{controller=Home}/{action=Index}/{id?}");
+        
+        // Redirect Route
+        app.MapControllerRoute(
+            name: "Redirect login",
+            pattern: "{controller=Login}/{action=Index}/{id?}",
+            defaults: new { area="Identity", controller = "Login", action = "Index" }
+        );
     }
 }
