2de0781ab9928f475e0694d997530849f0df3629
Ly Tran Vinh <88392742+Youknow2509@users.noreply.github.com>
2024-09-21 10:06:21 +0000

    [Upgrade]: api return token jwt when login

--- Apis/IdentityApiController.cs
+++ Apis/IdentityApiController.cs
@@ -1,18 +1,44 @@
 using Microsoft.AspNetCore.Mvc;
+using BCrypt.Net;
 //
 using qlsv.ViewModels;
+using qlsv.Helpers;
+
 namespace qlsv.Controllers;
 
 [Route("api/[controller]")]
 [ApiController]
 public class IdentityApiController : ControllerBase
-{
+{   
+    // Variables
+    private readonly qlsv.Data.IdentityDbContext _context;
+    private readonly JwtHelper _jwtHelper;
+
+    // Constructor
+    public IdentityApiController(
+        qlsv.Data.IdentityDbContext context,
+        JwtHelper jwtHelper
+    ) {
+        _context = context;
+        _jwtHelper = jwtHelper;
+    }
+    
     // POST: /api/Login/
     [HttpPost("login")]
     public ActionResult Login([FromBody] LoginViewModel model)
     {
-        // TODO code
-        return Ok("Api login route ok");
+        string passwordHash = BCrypt.Net.BCrypt.HashPassword(model.Password);
+        var user = _context.Users.FirstOrDefault(
+            u => 
+                u.UserName == model.UserNameOrEmail 
+                && u.PasswordHash == passwordHash
+        );
+        if (user != null)
+        {
+            string jwtToken = _jwtHelper.GenerateJwtToken(user.UserName);
+            return Ok(jwtToken);
+        }
+        return Ok("User and password not found");
     }
 }
 
