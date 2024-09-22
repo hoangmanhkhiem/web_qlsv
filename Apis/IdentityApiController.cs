a48022da306f7eb4a2c53b710cfcdca693e3ae3d
Ly Tran Vinh <88392742+Youknow2509@users.noreply.github.com>
2024-09-22 13:34:33 +0000

    [Upgrade]: Add cmt tester

--- Apis/IdentityApiController.cs
+++ Apis/IdentityApiController.cs
@@ -1,5 +1,4 @@
 using Microsoft.AspNetCore.Mvc;
-using BCrypt.Net;
 //
 using qlsv.ViewModels;
 using qlsv.Helpers;
@@ -13,21 +12,24 @@ public class IdentityApiController : ControllerBase
     // Variables
     private readonly qlsv.Data.IdentityDbContext _context;
     private readonly JwtHelper _jwtHelper;
+    private readonly SecurityHelper _sercurityHelper;
 
     // Constructor
     public IdentityApiController(
         qlsv.Data.IdentityDbContext context,
-        JwtHelper jwtHelper
+        JwtHelper jwtHelper,
+        SecurityHelper securityHelper  
     ) {
         _context = context;
         _jwtHelper = jwtHelper;
+        _sercurityHelper = securityHelper;
     }
     
     // POST: /api/Login/
     [HttpPost("login")]
     public ActionResult Login([FromBody] LoginViewModel model)
     {
-        string passwordHash = BCrypt.Net.BCrypt.HashPassword(model.Password);
+        string passwordHash = _sercurityHelper.Hash(model.Password);
         var user = _context.Users.FirstOrDefault(
             u => 
                 (u.UserName.ToUpper() == model.UserNameOrEmail.ToUpper() ||
@@ -40,6 +42,8 @@ public class IdentityApiController : ControllerBase
             return Ok(jwtToken);
         }
         return Ok("User and password not found");
+        // Test
+        // return Ok($"User Name: {model.UserNameOrEmail}, PasswordHash: {passwordHash}");
     }
 }
 
