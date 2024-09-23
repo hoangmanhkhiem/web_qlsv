8d01baccac9262302e19538bf134230e4676432b
Ly Tran Vinh <88392742+Youknow2509@users.noreply.github.com>
2024-09-23 07:26:07 +0000

    [Upgrade]: Add handel update users

--- Apis/IdentityApiController.cs
+++ Apis/IdentityApiController.cs
@@ -10,7 +10,7 @@ namespace qlsv.Controllers;
 [Route("api/[controller]")]
 [ApiController]
 public class IdentityApiController : ControllerBase
-{   
+{
     // Variables
     private readonly qlsv.Data.IdentityDbContext _context;
     private readonly JwtHelper _jwtHelper;
@@ -20,18 +20,21 @@ public class IdentityApiController : ControllerBase
     public IdentityApiController(
         qlsv.Data.IdentityDbContext context,
         JwtHelper jwtHelper,
-        SecurityHelper securityHelper  
-    ) {
+        SecurityHelper securityHelper
+    )
+    {
         _context = context;
         _jwtHelper = jwtHelper;
         _sercurityHelper = securityHelper;
     }
-    
+
     // GET: Users
     [HttpGet("users")]
-    public IActionResult GetUsers() {
+    public IActionResult GetUsers()
+    {
         var listUsers = _context.Users.ToList();
-        var userDtos = listUsers.Select(user => new UserCustom {
+        var userDtos = listUsers.Select(user => new UserCustom
+        {
             Id = user.Id,
             UserName = user.UserName,
             Email = user.Email,
@@ -47,9 +50,11 @@ public class IdentityApiController : ControllerBase
 
     // GET: Roles
     [HttpGet("roles")]
-    public IActionResult GetRoles() {
+    public IActionResult GetRoles()
+    {
         var listRole = _context.Roles.ToList();
-        var roleDtos = listRole.Select(role => new IdentityRole {
+        var roleDtos = listRole.Select(role => new IdentityRole
+        {
             Id = role.Id,
             Name = role.Name,
             NormalizedName = role.NormalizedName,
@@ -60,9 +65,11 @@ public class IdentityApiController : ControllerBase
 
     // GET: UserRoles
     [HttpGet("userroles")]
-    public IActionResult GetUserRoles() {
+    public IActionResult GetUserRoles()
+    {
         var listUserRoles = _context.UserRoles.ToList();
-        var userRoles = listUserRoles.Select(userRole => new IdentityUserRole<string> {
+        var userRoles = listUserRoles.Select(userRole => new IdentityUserRole<string>
+        {
             UserId = userRole.UserId,
             RoleId = userRole.RoleId,
         }).ToList();
@@ -71,9 +78,11 @@ public class IdentityApiController : ControllerBase
 
     // GET: RefreshTokens
     [HttpGet("refreshtokens")]
-    public IActionResult GetRefreshTokens() {
+    public IActionResult GetRefreshTokens()
+    {
         var listRefreshToken = _context.RefreshTokens.ToList();
-        var refreshTokens = listRefreshToken.Select(refreshToken => new RefreshToken {
+        var refreshTokens = listRefreshToken.Select(refreshToken => new RefreshToken
+        {
             Id = refreshToken.Id,
             UserId = refreshToken.UserId,
             Token = refreshToken.Token,
@@ -81,12 +90,14 @@ public class IdentityApiController : ControllerBase
         }).ToList();
         return Ok(refreshTokens);
     }
-    
+
     // GET: RoleClaims
     [HttpGet("roleclaims")]
-    public IActionResult GetRoleClaims() {
+    public IActionResult GetRoleClaims()
+    {
         var listRoleClaims = _context.RoleClaims.ToList();
-        var roleClaims = listRoleClaims.Select(roleClaim => new IdentityRoleClaim<string> {
+        var roleClaims = listRoleClaims.Select(roleClaim => new IdentityRoleClaim<string>
+        {
             Id = roleClaim.Id,
             RoleId = roleClaim.RoleId,
             ClaimType = roleClaim.ClaimType,
@@ -97,9 +108,11 @@ public class IdentityApiController : ControllerBase
 
     // GET: UserClaims
     [HttpGet("userclaims")]
-    public IActionResult GetUserClaims() {
+    public IActionResult GetUserClaims()
+    {
         var listUserClaims = _context.UserClaims.ToList();
-        var userClaims = listUserClaims.Select(userClaim => new IdentityUserClaim<string> {
+        var userClaims = listUserClaims.Select(userClaim => new IdentityUserClaim<string>
+        {
             Id = userClaim.Id,
             UserId = userClaim.UserId,
             ClaimType = userClaim.ClaimType,
@@ -110,9 +123,11 @@ public class IdentityApiController : ControllerBase
 
     // GET: UserLogins
     [HttpGet("userlogins")]
-    public IActionResult GetUserLogins() {
+    public IActionResult GetUserLogins()
+    {
         var listUserLogins = _context.UserLogins.ToList();
-        var userLogins = listUserLogins.Select(userLogin => new IdentityUserLogin<string> {
+        var userLogins = listUserLogins.Select(userLogin => new IdentityUserLogin<string>
+        {
             LoginProvider = userLogin.LoginProvider,
             ProviderKey = userLogin.ProviderKey,
             UserId = userLogin.UserId,
@@ -122,15 +137,40 @@ public class IdentityApiController : ControllerBase
 
     // GET: UserTokens
     [HttpGet("usertokens")]
-    public IActionResult GetUserTokens() {
+    public IActionResult GetUserTokens()
+    {
         var listUserTokens = _context.UserTokens.ToList();
-        var userTokens = listUserTokens.Select(userToken => new IdentityUserToken<string> {
+        var userTokens = listUserTokens.Select(userToken => new IdentityUserToken<string>
+        {
             UserId = userToken.UserId,
             LoginProvider = userToken.LoginProvider,
             Name = userToken.Name,
             Value = userToken.Value,
         }).ToList();
         return Ok(userTokens);
+    }
+
+    // POST: UpdateUser
+    [HttpPost("updateuser")]
+    public IActionResult UpdateUser(UserCutomInputJson model)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+        var user = _context.Users.FirstOrDefault(user => user.Id == model.Id);
+        if (user == null)
+        {
+            return NotFound("User not found");
+        }
+        user.UserName = model.UserName;
+        user.Email = model.Email;
+        user.FirstName = model.FirstName;
+        user.LastName = model.LastName;
+        user.Address = model.Address;
+        user.Phone = model.Phone;
+        _context.SaveChanges();
+        return Ok();
     }
 }
 
