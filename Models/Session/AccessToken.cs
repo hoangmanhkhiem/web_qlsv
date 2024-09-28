92c06cbf3b4775cf963c562a6950f523e4193570
V <lytranvinh.work@gmail.com>
2024-09-28 16:47:26 +0700

    [Upgrade]: Fix modle

--- Models/Session/AccessToken.cs
+++ Models/Session/AccessToken.cs
@@ -6,6 +6,5 @@ public class AccessToken
     public string Id { get; set; }
     public string Token { get; set; }
     public string UserId { get; set; }
-    public string RefreshTokenId { get; set; }
     public DateTime ExpiryDate { get; set; }
 }
--- Models/Session/RefreshToken.cs
+++ Models/Session/RefreshToken.cs
@@ -6,6 +6,6 @@ public class RefreshToken
     public string Id { get; set; }
     public string Token { get; set; }
     public string UserId { get; set; }
-    public bool IsRevoked { get; set; }
+    public bool IsActive { get; set; } = true;
     public DateTime ExpiryDate { get; set; }
 }
