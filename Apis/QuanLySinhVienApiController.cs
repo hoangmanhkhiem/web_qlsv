06e61e3f707a4338cf1ec4b4dd0ac6e3d9694c6c
V <lytranvinh.work@gmail.com>
2024-10-19 16:23:02 +0700

    [Upgrade]: add full rest api context quanLiSinhVienDbContext

--- Apis/QuanLySinhVienApiController.cs
+++ Apis/QuanLySinhVienApiController.cs
@@ -96,18 +96,95 @@ public class QuanLySinhVienApiController : ControllerBase
     }
 
     // POST: POST new data to table giao vien
+    [HttpPost("createGiaoVien")]
+    public IActionResult PostGiaoVien(GiaoVien giaoVien)
+    {
+        if (ModelState.IsValid)
+        {
+            _dbContext.GiaoViens.Add(giaoVien);
+            _dbContext.SaveChanges();
+            return Ok(giaoVien);
+        }
+        return BadRequest();
+    }
 
     // POST: POST new data to table sinh vien
+    [HttpPost("createSinhVien")]
+    public IActionResult PostSinhVien(SinhVien sinhVien)
+    {
+        if (ModelState.IsValid)
+        {
+            _dbContext.SinhViens.Add(sinhVien);
+            _dbContext.SaveChanges();
+            return Ok(sinhVien);
+        }
+        return BadRequest();
+    }
 
     // POST: POST new data to table thoi gian
+    [HttpPost("createThoiGian")]
+    public IActionResult PostThoiGian(ThoiGian thoiGian)
+    {
+        if (ModelState.IsValid)
+        {
+            _dbContext.ThoiGians.Add(thoiGian);
+            _dbContext.SaveChanges();
+            return Ok(thoiGian);
+        }
+        return BadRequest();
+    }
 
     // POST: POST new data to table lop hoc phan
+    [HttpPost("createLopHocPhan")]
+    public IActionResult PostLopHocPhan(LopHocPhan lopHocPhan)
+    {
+        if (ModelState.IsValid)
+        {
+            _dbContext.LopHocPhans.Add(lopHocPhan);
+            _dbContext.SaveChanges();
+            return Ok(lopHocPhan);
+        }
+        return BadRequest();
+    }
 
     // POST: POST new data to table diem danh
+    [HttpPost("createDiemDanh")]
+    public IActionResult PostDiemDanh(DiemDanh diemDanh)
+    {
+        if (ModelState.IsValid)
+        {
+            _dbContext.DiemDanhs.Add(diemDanh);
+            _dbContext.SaveChanges();
+            return Ok(diemDanh);
+        }
+        return BadRequest();
+    }
 
     // POST: POST new data to table sinh vien lop hoc phan
+    [HttpPost("createSinhVienLopHocPhan")]
+    public IActionResult PostSinhVienLopHocPhan(SinhVienLopHocPhan sinhVienLopHocPhan)
+    {
+        if (ModelState.IsValid)
+        {
+            _dbContext.SinhVienLopHocPhans.Add(sinhVienLopHocPhan);
+            _dbContext.SaveChanges();
+            return Ok(sinhVienLopHocPhan);
+        }
+        return BadRequest();
+    }
 
     // POST: POST new data to table thoi gian lop hoc phan
+    [HttpPost("createThoiGianLopHocPhan")]
+    public IActionResult PostThoiGianLopHocPhan(ThoiGianLopHocPhan thoiGianLopHocPhan)
+    {
+        if (ModelState.IsValid)
+        {
+            _dbContext.ThoiGianLopHocPhans.Add(thoiGianLopHocPhan);
+            _dbContext.SaveChanges();
+            return Ok(thoiGianLopHocPhan);
+        }
+        return BadRequest();
+    }
 
     // PUT: Update data in table diem
     [HttpPut("upgrateDiem")]
@@ -123,12 +200,95 @@ public class QuanLySinhVienApiController : ControllerBase
     }
 
     // PUT: Upgrade data to table giao vien
+    [HttpPut("upgrateGiaoVien")]
+    public IActionResult PutGiaoVien(GiaoVien giaoVien)
+    {
+        if (ModelState.IsValid)
+        {
+            _dbContext.GiaoViens.Update(giaoVien);
+            _dbContext.SaveChanges();
+            return Ok(giaoVien);
+        }
+        return BadRequest();
+    }
+
     // PUT: Upgrade data to table sinh vien
+    [HttpPut("upgrateSinhVien")]
+    public IActionResult PutSinhVien(SinhVien sinhVien)
+    {
+        if (ModelState.IsValid)
+        {
+            _dbContext.SinhViens.Update(sinhVien);
+            _dbContext.SaveChanges();
+            return Ok(sinhVien);
+        }
+        return BadRequest();
+    }
+
     // PUT: Upgrade data to table thoi gian
+    [HttpPut("upgrateThoiGian")]
+    public IActionResult PutThoiGian(ThoiGian thoiGian)
+    {
+        if (ModelState.IsValid)
+        {
+            _dbContext.ThoiGians.Update(thoiGian);
+            _dbContext.SaveChanges();
+            return Ok(thoiGian);
+        }
+        return BadRequest();
+    }
+
     // PUT: Upgrade data to table lop hoc phan
+    [HttpPut("upgrateLopHocPhan")]
+    public IActionResult PutLopHocPhan(LopHocPhan lopHocPhan)
+    {
+        if (ModelState.IsValid)
+        {
+            _dbContext.LopHocPhans.Update(lopHocPhan);
+            _dbContext.SaveChanges();
+            return Ok(lopHocPhan);
+        }
+        return BadRequest();
+    }
+
     // PUT: Upgrade data to table diem danh
+    [HttpPut("upgrateDiemDanh")]
+    public IActionResult PutDiemDanh(DiemDanh diemDanh)
+    {
+        if (ModelState.IsValid)
+        {
+            _dbContext.DiemDanhs.Update(diemDanh);
+            _dbContext.SaveChanges();
+            return Ok(diemDanh);
+        }
+        return BadRequest();
+    }
+
     // PUT: Upgrade data to table sinh vien lop hoc phan
+    [HttpPut("upgrateSinhVienLopHocPhan")]
+    public IActionResult PutSinhVienLopHocPhan(SinhVienLopHocPhan sinhVienLopHocPhan)
+    {
+        if (ModelState.IsValid)
+        {
+            _dbContext.SinhVienLopHocPhans.Update(sinhVienLopHocPhan);
+            _dbContext.SaveChanges();
+            return Ok(sinhVienLopHocPhan);
+        }
+        return BadRequest();
+    }
+
     // PUT: Upgrade data to table thoi gian lop hoc phan
+    [HttpPut("upgrateThoiGianLopHocPhan")]
+    public IActionResult PutThoiGianLopHocPhan(ThoiGianLopHocPhan thoiGianLopHocPhan)
+    {
+        if (ModelState.IsValid)
+        {
+            _dbContext.ThoiGianLopHocPhans.Update(thoiGianLopHocPhan);
+            _dbContext.SaveChanges();
+            return Ok(thoiGianLopHocPhan);
+        }
+        return BadRequest();
+    }
 
     // DELETE: Delete data in table diem
     [HttpDelete("deleteDiem/{id}")]
@@ -146,12 +306,108 @@ public class QuanLySinhVienApiController : ControllerBase
     }
 
     // DELETE: DELETE data to from table with id 
+    [HttpDelete("deleteGiaoVien/{id}")]
+    public IActionResult DeleteGiaoVien(string id)
+    {
+        var giaoVien = _dbContext.GiaoViens.FirstOrDefault(u => u.IdGiaoVien == id);
+        if (giaoVien == null)
+        {
+            return NotFound("GiaoVien not found");
+        }
+
+        _dbContext.GiaoViens.Remove(giaoVien);
+        _dbContext.SaveChanges();
+        return Ok($"GiaoVien {giaoVien.IdGiaoVien} deleted");
+    }
+
     // DELETE: DELETE data to from table with id 
+    [HttpDelete("deleteSinhVien/{id}")]
+    public IActionResult DeleteSinhVien(string id)
+    {
+        var sinhVien = _dbContext.SinhViens.FirstOrDefault(u => u.IdSinhVien == id);
+        if (sinhVien == null)
+        {
+            return NotFound("SinhVien not found");
+        }
+
+        _dbContext.SinhViens.Remove(sinhVien);
+        _dbContext.SaveChanges();
+        return Ok($"SinhVien {sinhVien.IdSinhVien} deleted");
+    }
+
     // DELETE: DELETE data to from table with id 
+    [HttpDelete("deleteThoiGian/{id}")]
+    public IActionResult DeleteThoiGian(string id)
+    {
+        var thoiGian = _dbContext.ThoiGians.FirstOrDefault(u => u.IdThoiGian == id);
+        if (thoiGian == null)
+        {
+            return NotFound("ThoiGian not found");
+        }
+
+        _dbContext.ThoiGians.Remove(thoiGian);
+        _dbContext.SaveChanges();
+        return Ok($"ThoiGian {thoiGian.IdThoiGian} deleted");
+    }
+
     // DELETE: DELETE data to from table with id 
+    [HttpDelete("deleteLopHocPhan/{id}")]
+    public IActionResult DeleteLopHocPhan(string id)
+    {
+        var lopHocPhan = _dbContext.LopHocPhans.FirstOrDefault(u => u.IdHocPhan == id);
+        if (lopHocPhan == null)
+        {
+            return NotFound("LopHocPhan not found");
+        }
+
+        _dbContext.LopHocPhans.Remove(lopHocPhan);
+        _dbContext.SaveChanges();
+        return Ok($"LopHocPhan {lopHocPhan.IdHocPhan} deleted");
+    }
     // DELETE: DELETE data to from table with id 
+    [HttpDelete("deleteDiemDanh/{id}")]
+    public IActionResult DeleteDiemDanh(string id)
+    {
+        var diemDanh = _dbContext.DiemDanhs.FirstOrDefault(u => u.IdDiemDanh == id);
+        if (diemDanh == null)
+        {
+            return NotFound("DiemDanh not found");
+        }
+
+        _dbContext.DiemDanhs.Remove(diemDanh);
+        _dbContext.SaveChanges();
+        return Ok($"DiemDanh {diemDanh.IdDiemDanh} deleted");
+    }
+
     // DELETE: DELETE data to from table with id  
+    [HttpDelete("deleteSinhVienLopHocPhan/{id}")]
+    public IActionResult DeleteSinhVienLopHocPhan(string id)
+    {
+        var sinhVienLopHocPhan = _dbContext.SinhVienLopHocPhans.FirstOrDefault(u => u.IdSinhVienLopHocPhan == id);
+        if (sinhVienLopHocPhan == null)
+        {
+            return NotFound("SinhVienLopHocPhan not found");
+        }
+
+        _dbContext.SinhVienLopHocPhans.Remove(sinhVienLopHocPhan);
+        _dbContext.SaveChanges();
+        return Ok($"SinhVienLopHocPhan {sinhVienLopHocPhan.IdSinhVienLopHocPhan} deleted");
+    }
+
     // DELETE: DELETE data to from table with id
+    [HttpDelete("deleteThoiGianLopHocPhan/{id}")]
+    public IActionResult DeleteThoiGianLopHocPhan(string id)
+    {
+        var thoiGianLopHocPhan = _dbContext.ThoiGianLopHocPhans.FirstOrDefault(u => u.IdThoiGianLopHocPhan == id);
+        if (thoiGianLopHocPhan == null)
+        {
+            return NotFound("ThoiGianLopHocPhan not found");
+        }
+
+        _dbContext.ThoiGianLopHocPhans.Remove(thoiGianLopHocPhan);
+        _dbContext.SaveChanges();
+        return Ok($"ThoiGianLopHocPhan {thoiGianLopHocPhan.IdThoiGianLopHocPhan} deleted");
+    }
 
     /**
      * List Api Adv
