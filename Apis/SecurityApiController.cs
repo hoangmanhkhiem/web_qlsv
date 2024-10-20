c61c6416c80bcffd41acc2f586ecdb2768ad17de
V <lytranvinh.work@gmail.com>
2024-10-21 02:03:23 +0700

    [Upgrade]: help debug data Encrypt, Dencrypt, Hash, ...

---
+++ Apis/SecurityApiController.cs
@@ -0,0 +1,62 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+
+//
+using qlsv.Helpers;
+
+namespace qlsv.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+[Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+public class SecurityApiController : ControllerBase
+{
+    // Variables
+    private readonly SecurityHelper _securityHelper;
+
+    // Constructor
+    public SecurityApiController(
+        SecurityHelper securityHelper
+    )
+    {
+        _securityHelper = securityHelper;
+    }
+    
+    /**
+     * POST: Encrypt data
+     */
+    [HttpPost("encrypt")]
+    public IActionResult Encrypt([FromBody] string data)
+    {
+        return Ok(_securityHelper.Encrypt(data));
+    }
+
+    /**
+     * POST: Decrypt data
+     */
+    [HttpPost("decrypt")]
+    public IActionResult Decrypt([FromBody] string data)
+    {
+        return Ok(_securityHelper.Decrypt(data));
+    }
+
+    /**
+     * POST: Hash data
+     */
+    [HttpPost("hash")]
+    public IActionResult Hash([FromBody] string data)
+    {
+        return Ok(_securityHelper.Hash(data));
+    }
+
+    /**
+     * POST: Verify hash
+     */
+    [HttpPost("verifyHash")]
+    public IActionResult Verify([FromBody] string data, [FromBody] string hash)
+    {
+        return Ok(_securityHelper.ValidateHash(data, hash));
+    }
+}
+
