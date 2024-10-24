eed952123bdb985ca3d444b662409b74efb24518
V <lytranvinh.work@gmail.com>
2024-10-24 15:15:11 +0700

    [Upgrade]: TODO up data table dang ki nguyen vong to compelete

--- ViewComponents/DataTablesViewComponent.cs
+++ ViewComponents/DataTablesViewComponent.cs
@@ -25,17 +25,32 @@ public class DataTablesViewComponent : ViewComponent
         switch (table.ToUpper())
         {
             case "SINHVIEN":
-                return View("SinhVien", await _context.SinhViens.ToListAsync());
+                return View("SinhVien", 
+                    await _context.SinhViens
+                        .Include(sv => sv.ChuongTrinhHocs)
+                        .ToListAsync());
             case "GIAOVIEN":
-                return View("GIAOVIEN");
+                return View("GIAOVIEN", 
+                    await _context.GiaoViens
+                        .Include(gv => gv.Khoas)
+                        .ToListAsync());
             case "LOPHOCPHAN":
-                return View("LopHocPhan");
+                return View("LopHocPhan", 
+                    await _context.LopHocPhans
+                        .Include(lhp => lhp.GiaoViens)
+                        .Include(lhp => lhp.MonHocs)
+                        .ToListAsync());
             case "CHUONGTRINHHOC":
-                return View("ChuongTrinhHoc", await _context.ChuongTrinhHocs.ToListAsync());
+                return View("ChuongTrinhHoc", 
+                    await _context.ChuongTrinhHocs.ToListAsync());
             case "KHOA":
-                return View("Khoa");
-            case "MonHoc":
-                return View("MonHoc");
+                return View("Khoa", 
+                    await _context.Khoas.ToListAsync());
+            case "MONHOC":
+                return View("MonHoc", 
+                    await _context.MonHocs
+                        .Include(mh => mh.Khoas)
+                        .ToListAsync());
             default:
                 return View();
         }
