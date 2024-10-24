4f4d911edace204125486a00f181eea79553dbfe
Youknow2509 <Youknow2509@users.noreply.github.com>
2024-10-24 07:29:43 +0000

    deploy: 32e3de0bc12a18c86d64f0593747d03ac6164750

--- wwwroot/sb-admin/js/demo/datatables-demo.js
+++ wwwroot/sb-admin/js/demo/datatables-demo.js
@@ -1,4 +1,10 @@
 // Call the dataTables jQuery plugin
 $(document).ready(function() {
-  $('#dataTable').DataTable();
+  $('#dataTable').DataTable(
+    {
+        language: {
+            url: '/sb-admin/json/data_tables_vi.json',
+        },
+    }
+  );
 });
