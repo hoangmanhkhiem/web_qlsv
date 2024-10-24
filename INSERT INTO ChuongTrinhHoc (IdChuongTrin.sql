28730733b7d59f03f9ab3fd7c80025a1667460ec
Hoang Manh Khiem <khiemhm04@gmail.com>
2024-10-24 09:33:37 +0000

    hehehehehehehehe

---
+++ INSERT INTO ChuongTrinhHoc (IdChuongTrin.sql
@@ -0,0 +1,6 @@
+INSERT INTO ChuongTrinhHoc (IdChuongTrinhHoc, TenChuongTrinhHoc)
+VALUES (NEWID(), N'CLC Công nghệ thông tin'),
+       (NEWID(), N'Công nghệ thông tin'),
+       (NEWID(), N'Khoa học máy tính'),
+       (NEWID(), N'Toán ứng dụng'),
+       (NEWID(), N'Logicstic và quản lý chuỗi cung ứng');
---
+++ INSERT INTO Khoa (IdKhoa, TenKhoa).sql
@@ -0,0 +1,6 @@
+INSERT INTO Khoa (IdKhoa, TenKhoa)
+VALUES (NEWID(), N'Công nghệ thông tin'),
+       (NEWID(), N'Kinh tế'),
+       (NEWID(), N'Khoa học cơ bản'),
+       (NEWID(), N'Cầu đường'),
+       (NEWID(), N'Đào tạo quốc tế');
