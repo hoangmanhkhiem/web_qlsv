15ac5e78537b572a8bdae5e9bdba2d355fee08ad
V <lytranvinh.work@gmail.com>
2024-10-24 15:07:00 +0700

    [Fix]: handle link loc hop phan voi mon hoc

--- Migrations/QuanLySinhVienDb/20241024075735_link_lhp_mh.Designer.cs
+++ Migrations/QuanLySinhVienDb/20241024075735_link_lhp_mh.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using qlsv.Data;
 
@@ -11,9 +12,11 @@ using qlsv.Data;
 namespace web_qlsv.Migrations.QuanLySinhVienDb
 {
     [DbContext(typeof(QuanLySinhVienDbContext))]
-    partial class QuanLySinhVienDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241024075735_link_lhp_mh")]
+    partial class link_lhp_mh
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/QuanLySinhVienDb/20241024075735_link_lhp_mh.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace web_qlsv.Migrations.QuanLySinhVienDb
+{
+    /// <inheritdoc />
+    public partial class link_lhp_mh : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_LopHocPhan_IdMonHoc",
+                table: "LopHocPhan",
+                column: "IdMonHoc");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_LopHocPhan_MonHoc_IdMonHoc",
+                table: "LopHocPhan",
+                column: "IdMonHoc",
+                principalTable: "MonHoc",
+                principalColumn: "IdMonHoc");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_LopHocPhan_MonHoc_IdMonHoc",
+                table: "LopHocPhan");
+
+            migrationBuilder.DropIndex(
+                name: "IX_LopHocPhan_IdMonHoc",
+                table: "LopHocPhan");
+        }
+    }
+}
