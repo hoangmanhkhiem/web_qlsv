c0c579553d4f35e2d15a7712d2304c04c1286ea7
Ly Tran Vinh <88392742+Youknow2509@users.noreply.github.com>
2024-10-27 18:00:40 +0000

    [Upgrade]: add nguyen vong api

--- Apis/qlsv/NguyenVongController.cs
+++ Apis/qlsv/NguyenVongController.cs
@@ -15,17 +15,87 @@ namespace qlsv.Controllers;
 public class NguyenVongController : ControllerBase
 {
     // Variables
-    private readonly qlsv.Data.IdentityDbContext _context;
-    private readonly QuanLySinhVienDbContext _quanLySinhVienDbContext;
+    private readonly QuanLySinhVienDbContext _context;
 
     // Constructor
     public NguyenVongController(
-        qlsv.Data.IdentityDbContext context,
         QuanLySinhVienDbContext quanLySinhVienDbContext)
     {
-        _context = context;
-        _quanLySinhVienDbContext = quanLySinhVienDbContext;
+        _context = quanLySinhVienDbContext;
     }
-    // TODO
+
+    /**
+     * GET: api/nguyenvong/
+     * Get all nguyen vong 
+     */
+    [HttpGet]
+    public async Task<IActionResult> GetNguyenVongs()
+    {
+        var nguyenVongs = (
+            from nv in _context.DangKyNguyenVongs
+            join sv in _context.SinhViens on nv.IdSinhVien equals sv.IdSinhVien
+            join mh in _context.MonHocs on nv.IdMonHoc equals mh.IdMonHoc
+            select new
+            {
+                IdDangKyNguyenVong = nv.IdDangKyNguyenVong,
+                IdSinhVien = nv.IdSinhVien,
+                IdMonHoc = mh.IdMonHoc,
+                TenSinhVien = sv.HoTen,
+                TenMonHoc = mh.TenMonHoc,
+            }
+        ).ToList();
+
+        return Ok(nguyenVongs);
+    }
+
+    /**
+     * GET: api/nguyenvong/{id}/
+     * get nguyen vong by id
+     */
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetNguyenVongById(string id)
+    {
+        var nguyenVongs = (
+            from nv in _context.DangKyNguyenVongs
+            where nv.IdDangKyNguyenVong == id
+            join sv in _context.SinhViens on nv.IdSinhVien equals sv.IdSinhVien
+            join mh in _context.MonHocs on nv.IdMonHoc equals mh.IdMonHoc
+            select new
+            {
+                IdDangKyNguyenVong = nv.IdDangKyNguyenVong,
+                IdSinhVien = nv.IdSinhVien,
+                IdMonHoc = mh.IdMonHoc,
+                TenSinhVien = sv.HoTen,
+                TenMonHoc = mh.TenMonHoc,
+            }
+        ).ToList();
+
+        return Ok(nguyenVongs);
+    }
+
+    /**
+     * GET: api/nguyenvong/sinhvien/{id}/
+     * get nguyen vong by sinh vien id
+     */
+    [HttpGet("sinhvien/{id}")]
+    public async Task<IActionResult> GetNguyenVongBySinhVien(string id)
+    {
+        var nguyenVongs = (
+            from nv in _context.DangKyNguyenVongs
+            where nv.IdSinhVien == id
+            join sv in _context.SinhViens on nv.IdSinhVien equals sv.IdSinhVien
+            join mh in _context.MonHocs on nv.IdMonHoc equals mh.IdMonHoc
+            select new
+            {
+                IdDangKyNguyenVong = nv.IdDangKyNguyenVong,
+                IdSinhVien = nv.IdSinhVien,
+                IdMonHoc = mh.IdMonHoc,
+                TenSinhVien = sv.HoTen,
+                TenMonHoc = mh.TenMonHoc,
+            }
+        ).ToList();
+
+        return Ok(nguyenVongs);
+    }
 }
 
