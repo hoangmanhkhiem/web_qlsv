0f23b913812be8010151846ae5e33c7feda43913
V <lytranvinh.work@gmail.com>
2024-10-28 22:42:47 +0700

    [upgadate]: handle api avt

--- Apis/AvatarController.cs
+++ Apis/AvatarController.cs
@@ -28,22 +28,69 @@ public class AvatarController : ControllerBase
     [HttpGet("user/{idUserClaim}")]
     public async Task<IActionResult> GetAvatar(string idUserClaim)
     {
-        byte[] bytes = (from a in _context.Users
-                        where a.IdClaim == idUserClaim
-                        select a.ProfilePicture
-                        ).FirstOrDefault();
-
-        if (bytes == null)
+        var stringBase64 = (
+            from us in _context.Users
+            where us.IdClaim == idUserClaim
+            select new {
+                ProfilePictureBase64 = us.ProfilePictureBase64
+            }
+        ).FirstOrDefault();
+        if (stringBase64 == null)
         {
-            return NotFound("Avatar not found");
+            return NotFound();
         }
+        return Ok(stringBase64);
+    }
 
-        // Serialize the byte array to JSON
-        var json = JsonSerializer.Serialize(bytes);
+    // PUT: Update avatar for user with idUserClaim
+    [HttpPut("user/{idUserClaim}")]
+    public async Task<IActionResult> PutAvatar(string idUserClaim, [FromBody] string stringBase64)
+    {
+        var user = (
+            from us in _context.Users
+            where us.IdClaim == idUserClaim
+            select us
+        ).FirstOrDefault();
+        if (user == null)
+        {
+            return NotFound();
+        }
+        // Check if stringBase64 is img/png
+        if (stringBase64 == null)
+        {
+            return BadRequest();
+        }
+        if (stringBase64.Length < 22)
+        {
+            return BadRequest();
+        }
+        if (stringBase64.Substring(0, 22) != "data:image/png;base64,")
+        {
+            return BadRequest("Invalid base64 string parameter format img/png");  
+        }
+        user.ProfilePictureBase64 = stringBase64;
+        _context.Entry(user).State = EntityState.Modified;
+        await _context.SaveChangesAsync();
+        return NoContent();
+    }
 
-        return Ok(
-            json
-        );
+    // DELETE: Delete avatar for user with idUserClaim
+    [HttpDelete("user/{idUserClaim}")]
+    public async Task<IActionResult> DeleteAvatar(string idUserClaim)
+    {
+        var user = (
+            from us in _context.Users
+            where us.IdClaim == idUserClaim
+            select us
+        ).FirstOrDefault();
+        if (user == null)
+        {
+            return NotFound();
+        }
+        user.ProfilePictureBase64 = null;
+        _context.Entry(user).State = EntityState.Modified;
+        await _context.SaveChangesAsync();
+        return NoContent();
     }
 }
 
