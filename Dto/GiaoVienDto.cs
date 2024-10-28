a6006b930257879f11582d246188348e84adb95f
V <lytranvinh.work@gmail.com>
2024-10-28 22:03:52 +0700

    [fix]: changle to dir

--- Dto/GiaoVienDto.cs
+++ Dto/GiaoVienDto.cs
@@ -1,7 +1,8 @@
-namespace qlsv.ViewModels.dto
+namespace qlsv.Dto
 {
     public class GiaoVienDto
     {
+        public string IdGiaoVien { get; set; }
         public string TenGiaoVien { get; set; }
         public string Email { get; set; }
         public string SoDienThoai { get; set; }
--- Dto/UpdatePasswordDto.cs
+++ Dto/UpdatePasswordDto.cs
@@ -1,7 +1,7 @@
 
 using System.ComponentModel.DataAnnotations;
 
-namespace qlsv.ViewModels.dto
+namespace qlsv.Dto
 {
     public class UpdatePasswordDto
     {
