31b2ee90a26f6ee022e54e48f6999807a2750cb9
Hoang Manh Khiem <khiemhm04@gmail.com>
2024-11-03 00:45:45 +0700

    Update SinhVienDto.cs

--- Dto/SinhVienDto.cs
+++ Dto/SinhVienDto.cs
@@ -32,19 +32,5 @@ namespace qlsv.Dto
 
         [Required(ErrorMessage = "Không được để trống")]
         public string? TenKhoa { get; set; }
-
-        [Required(ErrorMessage = "Không được để trống")]
-
-        public string? IdSinhVien { get; set; }
-        public string? IdKhoa { get; set; }
-        public string? IdChuongTrinhHoc { get; set; }
-        //
-        public string? HoTen { get; set; }
-        public string? Lop { get; set; }
-        public DateTime? NgaySinh { get; set; }
-        public string? DiaChi { get; set; }
-        public string? TenKhoa { get; set; }
-
-        public string? TenChuongTrinhHoc { get; set; }
     }
 }
